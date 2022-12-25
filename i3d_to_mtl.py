44b7b28ac8a0de5db123ec57b190768127db5587
VidhosticeSDK <106232621+VidhosticeSDK@users.noreply.github.com>
2022-12-25 03:16:01 +0100

    Add files via upload

--- i3d_to_mtl.py
+++ i3d_to_mtl.py
@@ -2,24 +2,39 @@ import sys
 import untangle
 #pip install untangle
 
+
+FS22_data = 'c:/FS22/Data_FS22/data'
+
+
 obj = untangle.parse(sys.argv[1])
 
 def fileId_Texture_from_materialId(materialId):
     for material in obj.i3D.Materials.Material:
-        if material['materialId'] == materialId:
-            return material.Texture['fileId']
+        if 'Texture' in dir(material):
+            if material['materialId'] == materialId:
+                return material.Texture['fileId']
+        else:
+            return None
 
 def fileId_Normalmap_from_materialId(materialId):
     for material in obj.i3D.Materials.Material:
-        if material['materialId'] == materialId:
-            return material.Normalmap['fileId']
+        if 'Normalmap' in dir(material):
+            if material['materialId'] == materialId:
+                return material.Normalmap['fileId']
+        else:
+            return None
 
 def fileId_Glossmap_from_materialId(materialId):
     for material in obj.i3D.Materials.Material:
-        if material['materialId'] == materialId:
-            return material.Glossmap['fileId']
+        if 'Glossmap' in dir(material):
+            if material['materialId'] == materialId:
+                return material.Glossmap['fileId']
+        else:
+            return None
 
 def Filename_from_fileId(fileId):
+    if fileId is None:
+        return None
     for file in obj.i3D.Files.File:
         if file['fileId'] == fileId:
             return file['filename']
@@ -36,10 +51,21 @@ for shape in obj.i3D.Scene.Shape:
     i = 1
     for material in materials.split(","):
         print('newmtl', i)
-        print('map_Kd '+Filename_from_fileId(fileId_Texture_from_materialId(material)))
-        print('map_Bump '+Filename_from_fileId(fileId_Normalmap_from_materialId(material)))
-        print('map_Ks '+Filename_from_fileId(fileId_Glossmap_from_materialId(material)))
+
+        texture = Filename_from_fileId(fileId_Texture_from_materialId(material))
+        if texture is not None:
+            print('map_Kd '+texture.replace('$data', FS22_data))
+
+        normalmap = Filename_from_fileId(fileId_Normalmap_from_materialId(material))
+        if normalmap is not None:
+            print('map_Bump '+normalmap.replace('$data', FS22_data))
+
+        glossmap = Filename_from_fileId(fileId_Glossmap_from_materialId(material))
+        if glossmap is not None:
+            print('map_Ks '+glossmap.replace('$data', FS22_data))
+
         i = i + 1
+
     sys.stdout = sys.__stdout__
     file.close()
     print('Done')
